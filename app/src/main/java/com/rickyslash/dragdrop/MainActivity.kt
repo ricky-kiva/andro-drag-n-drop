b52c3a95fac7a73ad50dec4c51472d1f83de1389
ricky-kiva <ricky.kiva@gmail.com>
2023-09-22 01:08:47 +0700

    feat(activity-main): add drag n drop item positioning

--- app/src/main/java/com/rickyslash/dragdrop/MainActivity.kt
+++ app/src/main/java/com/rickyslash/dragdrop/MainActivity.kt
@@ -25,7 +25,33 @@ class MainActivity : AppCompatActivity() {
         binding.llBottom.setOnDragListener(dragListener)
 
         binding.viewBall.setOnLongClickListener {
-            val clipText = "Goal! Good job Messi!"
+            val clipText = "Goal! Good job Messi!" // content for the data
+            val item = ClipData.Item(clipText) // contains the data that will be drag & dropped (placed on clipboard)
+            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)  // type of data that is being put to the clipboard
+            val data = ClipData(clipText, mimeTypes, item) // packages of the data to copy
+
+            val dragShadow = View.DragShadowBuilder(it) // create shadow during drag & drop
+            it.startDragAndDrop(data, dragShadow, it, 0) // initiate the drag & drop operation
+
+            it.visibility = View.INVISIBLE // set this object to be invisible while drag & dropped
+            true
+        }
+
+        binding.viewBallTwo.setOnLongClickListener {
+            val clipText = "Goal! Good job Ronaldo!"
+            val item = ClipData.Item(clipText)
+            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            val data = ClipData(clipText, mimeTypes, item)
+
+            val dragShadow = View.DragShadowBuilder(it)
+            it.startDragAndDrop(data, dragShadow, it, 0)
+
+            it.visibility = View.INVISIBLE
+            true
+        }
+
+        binding.viewBallThree.setOnLongClickListener {
+            val clipText = "Goal! Good job Ronaldo!"
             val item = ClipData.Item(clipText)
             val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
             val data = ClipData(clipText, mimeTypes, item)
@@ -38,43 +64,91 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // `v` is the destination view
+    // `event` encapsulates information about the drag-n-drop operation
     private val dragListener = View.OnDragListener { v, event ->
         when(event.action) {
+
+            // when drag operation starts, this triggered
             DragEvent.ACTION_DRAG_STARTED -> {
+                // check if the dragged data has a MIME type of plain text, not continuing the process if false
                 event.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
             }
 
+            // when dragged item enters the bounds of the view, this triggered
             DragEvent.ACTION_DRAG_ENTERED -> {
-                v.invalidate()
+                v.invalidate() // redraw the view
                 true
             }
 
+            // when drag location changes but no other specific action is needed, this trigggered
             DragEvent.ACTION_DRAG_LOCATION -> true
 
+            // when drag leaves the bounds of the view, this triggered
             DragEvent.ACTION_DRAG_EXITED -> {
-                v.invalidate()
+                v.invalidate() // redraw the view
                 true
             }
 
+            // when user release the dragged item, this triggered
             DragEvent.ACTION_DROP -> {
-                val item = event.clipData.getItemAt(0)
-                val dragData = item.text
+                val item = event.clipData.getItemAt(0) // retrieve the first item from ClipData
+                val dragData = item.text // get first item content
                 Toast.makeText(this, dragData, Toast.LENGTH_SHORT).show()
 
-                v.invalidate()
+                v.invalidate() // redraw the view (call this before making changes in view's layout)
 
-                val dragObject = event.localState as View
-                val dragParent = dragObject.parent as ViewGroup
-                dragParent.removeView(dragObject)
+                val dragObject = event.localState as View // get the dragged item view
+                val dragParent = dragObject.parent as ViewGroup // get the dragged item parent
+                dragParent.removeView(dragObject) // remove the dragged item from its parent
 
-                val destination = v as LinearLayout
-                destination.addView(dragObject)
-                dragObject.visibility = View.VISIBLE
+                val destination = v as LinearLayout // set destination view as linear layout
+
+                val x = event.x
+                val y = event.y
+
+                val childCount = destination.childCount
+                var index = -1
+
+                if (childCount > 0) {
+                    val firstChild = destination.getChildAt(0)
+                    val lastChild = destination.getChildAt(childCount - 1)
+                    val childHalfHeight = firstChild.height / 2
+                    val childHalfWidth = firstChild.width / 2
+
+                    if (y < firstChild.y + childHalfHeight && x < firstChild.x + childHalfWidth) {
+                        index = 0 // Drop position is to the left of the first child
+                    } else if (y > lastChild.y + childHalfHeight && x > lastChild.x + childHalfWidth) {
+                        index = childCount // Drop position is to the right of the last child
+                    } else {
+                        for (i in childCount - 1 downTo 0) {
+                            val child = destination.getChildAt(i)
+                            val childHalfHeight = child.height / 2
+                            val childHalfWidth = child.width / 2
+
+                            if (y < child.y + childHalfHeight && x < child.x + childHalfWidth) {
+                                index = i
+                                break
+                            }
+                        }
+                    }
+                }
+
+                if (index != -1) {
+                    destination.addView(dragObject, index)
+                } else {
+                    destination.addView(dragObject)
+                }
+
+                // destination.addView(dragObject) // add dragged item to the destination
+
+                dragObject.visibility = View.VISIBLE // change visibility of dragged object
                 true
             }
 
+            // when drag operation ends
             DragEvent.ACTION_DRAG_ENDED -> {
-                v.invalidate()
+                v.invalidate() // redraw the view
                 true
             }
 
@@ -82,3 +156,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+/* Note: Adding multiple items
+
+val clipText1 = "Item 1"
+val clipText2 = "Item 2"
+
+val item1 = ClipData.Item(clipText1)
+val item2 = ClipData.Item(clipText2)
+
+val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+
+val data = ClipData("Multiple Items", mimeTypes, item1)
+data.addItem(item2)
+*/
