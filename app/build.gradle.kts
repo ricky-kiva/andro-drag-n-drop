f9325d42dd12e7d46cf5d26484851d9bfd295b10
ricky-kiva <ricky.kiva@gmail.com>
2023-09-22 13:07:00 +0700

    build(gradle): add FlexboxLayout dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,12 +41,15 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("net.objecthunter:exp4j:0.4.8")
+
+    implementation("net.objecthunter:exp4j:0.4.8") // math expression handling
+
+    implementation("com.google.android.flexbox:flexbox:3.0.0") // flexbox layout
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
